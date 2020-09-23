9a3901f39b29c0c12b53de6abbde4a154b0bda3f
颜钦财 <yanqincai@yopoun.com>
2020-09-23 15:28:13 +0800

    修改操作

--- src/pages/HomeNew.vue
+++ src/pages/HomeNew.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="orderSystem">
     <div>
-      <group label-width="5em">
+      <group>
+        <div style="color: #333; font-size: 3em;text-align: center;padding-top: 0.1em;margin-bottom: 0.2em;">报单系统</div>
+      </group>
+      <group label-width="6em">
         <group >
           <popup-picker class="fontStyle"  title="下单平台" :data="orderMallOptions"
                         :columns="1" v-model="dataForm.orderMall" show-name placeholder="请选择下单平台"></popup-picker>
@@ -13,34 +16,38 @@
         </group>
 
         <group >
-          <x-input class="fontStyle" text-align="right" title="订单号" v-model="dataForm.orderNum" required="true" placeholder="请输入订单号"></x-input>
+          <x-input class="fontStyle" text-align="right" title="订单号" v-model="dataForm.orderNum" required placeholder="请输入订单号"></x-input>
         </group>
 
         <group >
-          <x-input class="fontStyle" text-align="right" title="运单号" v-model="dataForm.trackingNumber" required="true" placeholder="请输入运单号"></x-input>
+          <x-input class="fontStyle" text-align="right" title="运单号" v-model="dataForm.trackingNumber" required placeholder="请输入运单号"></x-input>
         </group>
 
         <group >
-          <x-input class="fontStyle" text-align="right" title="收货人姓名" v-model="dataForm.consigneeName" required="true" is-type="china-name" placeholder="请输入收货人姓名"></x-input>
+          <x-input class="fontStyle" text-align="right" title="收货人姓名" v-model="dataForm.consigneeName" required is-type="china-name" placeholder="请输入收货人姓名"></x-input>
         </group>
 
         <group >
-          <x-input class="fontStyle" text-align="right" title="收货人电话" v-model="dataForm.consigneePhone" is-type="china-mobile" required="true" placeholder="请输入收货人电话"></x-input>
+          <x-input class="fontStyle" text-align="right" title="收货人电话" v-model="dataForm.consigneePhone" is-type="china-mobile" required placeholder="请输入收货人电话"></x-input>
         </group>
 
         <group >
-          <x-input class="fontStyle" text-align="right" title="抢购手机型号" v-model="dataForm.phoneModel" required="true" placeholder="请输入抢购手机型号"></x-input>
+          <x-input class="fontStyle" text-align="right" title="抢购手机型号" v-model="dataForm.phoneModel" required placeholder="请输入抢购手机型号"></x-input>
         </group>
 
         <group >
-          <x-input class="fontStyle" text-align="right" title="支付金额" v-model="dataForm.paidAmount" type="number" required="true" placeholder="请输入实际支付金额（成本价）"></x-input>
+          <x-input class="fontStyle" text-align="right" title="支付金额" v-model="dataForm.paidAmount" type="number" required placeholder="请输入实际支付金额（成本价）"></x-input>
         </group>
 
         <group>
           <x-address class="fontStyle" title="下货地区" :list="addressData" v-model="dataForm.deliveryArea" placeholder="请选择地址"></x-address>
         </group>
+
+        <group class="fontStyleSmall" style="color: red; width: 95%; text-align: center;">
+          <p >注意：1个单子默认一个订单的机子，拆单的拆着报，多单的多提交报单</p>
+        </group>
         <group>
-          <x-button plain type="primary" style="border-radius:99px;">primary</x-button>
+        <x-button type="primary" style="margin: 1.5em auto 0;border-radius:99px; width: 95%; height: 3.5em; letter-spacing: 0.1em; position: relative;">提交</x-button>
         </group>
 
       </group>
@@ -54,7 +61,8 @@
 </i18n>
 
 <script>
-import { PopupPicker, Group, XInput, ChinaAddressV4Data, XAddress, XButton} from 'vux'
+// import { PopupPicker, Group, XInput, ChinaAddressV4Data, XAddress, XButton} from 'vux'
+import { PopupPicker, Group, XInput, ChinaAddressV4Data, XAddress, XButton } from 'vux'
 export default {
   data () {
     return {
@@ -100,7 +108,7 @@ export default {
         consigneePhone: '',
         phoneModel: '',
         paidAmount: '',
-        deliveryArea: ''
+        deliveryArea: []
       }
 
     }
@@ -135,7 +143,13 @@ export default {
   }
   .fontStyle {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    font-size: 2em;
+    font-size: 32px;
     font-weight: 400;
+  }
+
+  .fontStyleSmall {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-size: 32px;
+    font-weight: 300;
   }
 </style>
